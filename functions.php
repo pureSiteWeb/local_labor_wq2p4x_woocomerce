d09d29aefb27c2c715dedbeee063293f89b4a351
pureSiteWeb <75913310+pureSiteWeb@users.noreply.github.com>
2020-12-15 19:44:19 +0100

    pontositas

--- functions.php
+++ functions.php
@@ -4,34 +4,46 @@
  * Add the checkbox & tax number fields to the checkout
  */
 
-// add 'checkbox' field
-add_action( 'woocommerce_after_checkout_billing_form', 'personal_customer_checkbox' );
+add_filter( 'woocommerce_checkout_fields' , 'new_tax_number_checkout_field' );
+  
+function new_tax_number_checkout_field( $fields ) {
+    $fields['billing']['checkbox_personak_customer'] = array(
+        'type'      => 'checkbox',
+        'label'     => __('Céges vásárlás', 'woocommerce'),
+        'class'     => array('form-row-wide'),
+        'clear'     => true
+    );
+    $fields['billing']['tax_number_field'] = array(
+        'label'     => __('Adószám', 'woocommerce'),
+        'class'     => array('form-row-wide'),
+        'clear'     => true,
+        'required'  => true
+    );
+    
+    return $fields;
 
-// checkbox field
-function personal_customer_checkbox( $checkout){
-    woocommerce_form_field( 'personal_customer' , array(
-         'type'    => 'checkbox',
-         'class'   => array('woocommerce-form__input woocommerce-form__input-checkbox input-checkbox'),
-         'label'	=> 'Céges vásárlás',
-    ), $checkout->get_value( 'personal_customer' ));
 }
 
-
-// add 'tax number' field
-add_action( 'woocommerce_after_checkout_billing_form', 'tax_number_checkout_field' );
-
-// input field
-function tax_number_checkout_field( $checkout ) {
-     
-    echo '<div class="tax_number" id="tax_number_data" style="display: none;">';
-     woocommerce_form_field( 'tax_number_field', array(
-          'type'    => 'text',
-          'required'     => true,
-          'class'   => array('form-row-wide'),
-          'label'   => __('Adószám'),
-     ), $checkout->get_value( 'tax_number_field' ));
-     echo '</div>';
-
+// hide if not checked
+add_action( 'woocommerce_after_checkout_form', 'tax_number_hide', 9999 );
+  
+function tax_number_hide() {
+    
+  wc_enqueue_js( "
+      jQuery('input#checkbox_personak_customer').change(function(){
+           
+         if (! this.checked) {
+            // HIDE IF NOT CHECKED
+            jQuery('#tax_number_field_field').fadeOut();
+            jQuery('#tax_number_field_field input').val('');         
+         } else {
+            // SHOW IF CHECKED
+            jQuery('#tax_number_field_field').fadeIn();
+         }
+           
+      }).change();
+  ");
+       
 }
 
 /**
@@ -47,29 +59,17 @@ function tax_number_checkout_field_update_order_meta( $order_id ) {
 }
 
 
-/**
- * Process the checkout
- */
+//--
 
-add_action('woocommerce_checkout_process', 'tax_number_checkout_field_process');
-
-function tax_number_checkout_field_process() {
-    // Check if set, if its not set add an error.
-    if ( ! $_POST['tax_number_field'] )
-        wc_add_notice( __( 'Céges vásárlás esetén adószám kitöltése kötelező' ), 'error' );
-}
 
 /**
  * Display field value on the order edit page
  */
-add_action( 'woocommerce_admin_order_data_after_billing_address', 'tax_number_checkout_field_display_admin_order_meta', 10, 1 );
+ 
+add_action( 'woocommerce_admin_order_data_after_shipping_address', 'my_custom_checkout_field_display_admin_order_meta', 10, 1 );
 
-function tax_number_checkout_field_display_admin_order_meta($order){
-    echo '<p><strong>'.__('tax_number_field').':</strong> ' . get_post_meta( $order->id, 'tax_number_field', true ) . '</p>';
+function my_custom_checkout_field_display_admin_order_meta($order){
+    echo '<p><strong>'.__('Adószám').':</strong> ' . get_post_meta( $order->get_id(), '_tax_number_field', true ) . '</p>';
 }
-
-<script>
-<console class="log"></console>
-</script>
 
 ?>
