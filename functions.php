903a1242a6335a52c1af96f552dbb4a61750c6c6
pureSiteWeb <75913310+pureSiteWeb@users.noreply.github.com>
2020-12-15 13:21:08 +0100

    adoszam mezo megjelenitve
    Adding Custom Shipping And Billing Fields

--- functions.php
+++ functions.php
@@ -1,42 +1,24 @@
 <?php
 
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+// source: https://docs.woocommerce.com/document/tutorial-customising-checkout-fields-using-actions-and-filters/
+
+/**
+ * Add tax number field in the 'Checkout' page
+ */
+
+add_filter( 'woocommerce_checkout_fields' , 'tax_number_field_checkout' );
 
 // Our hooked in function – $fields is passed via the filter!
-function custom_override_checkout_fields( $fields ) {
-     $fields['billing']['billing_anumber'] = array(
-        'label'     => __('Adószám', 'woocommerce'),
-    'placeholder'   => _x('Adószám', 'placeholder', 'woocommerce'),
-    'required'  => true,
-    'class'     => array('form-row-wide'),
-    'clear'     => true
+function tax_number_field_checkout( $fields ) {
+     $fields['billing']['tax_number'] = array(
+          'label'     => __('Adószám', 'woocommerce'),
+          'required'  => true,
+          'class'     => array('form-row-wide'),
+          'clear'     => true
+          // 'priority'  => 20
      );
 
      return $fields;
 }
-
-
-/**
- * Display field value on the order edit page
- */
- 
-add_action( 'woocommerce_thankyou', 'my_custom_view_order', 20 );
-
-function my_custom_view_order($order){
-    //echo '<p><strong>'.__('Adószám').':</strong> ' . get_post_meta( $order->get_id(), '_billing_anumber', true ) . '</p>';
-    echo "LOL";
-}
-
-/**
- * Display field value on the order edit page
- */
- 
-add_action( 'woocommerce_admin_order_data_after_shipping_address', 'my_custom_checkout_field_display_admin_order_meta', 10, 1 );
-
-function my_custom_checkout_field_display_admin_order_meta($order){
-    echo '<p><strong>'.__('Adószám').':</strong> ' . get_post_meta( $order->get_id(), '_billing_anumber', true ) . '</p>';
-}
-
-
 
 ?>
